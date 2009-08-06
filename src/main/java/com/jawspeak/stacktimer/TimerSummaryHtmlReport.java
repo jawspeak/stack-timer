902484dfd4c74f93f7b0b16f86b2531f5363cba6
Jonathan Andrew Wolter <jawolter@gmail.com>
2009-08-05 22:26:00 -0500

    fixed the spacing for tieline html writer for summaries

--- src/main/java/com/jawspeak/stacktimer/TimerSummaryHtmlReport.java
+++ src/main/java/com/jawspeak/stacktimer/TimerSummaryHtmlReport.java
@@ -2,6 +2,7 @@ package com.jawspeak.stacktimer;
 
 public class TimerSummaryHtmlReport {
 
+  private static final int BAR_LINE_HEIGHT = 20;
   private final TimerSummary timerSummary;
   private StringBuilder sb = new StringBuilder();
 
@@ -19,57 +20,39 @@ public class TimerSummaryHtmlReport {
   private void renderHeader() {
     sb.append("<style>\n" + 
     		"#chart-stats {\n" + 
-    		"    background-color: #f1f1f1;\n" + 
-    		"    margin: auto;\n" + 
-    		"    padding: 3px;\n" + 
-    		"    width: 1000px;\n" + 
+    		"    position: relative;\n" + 
     		"\n" + 
     		"}\n" + 
     		".timeline {\n" + 
-    		"    position: relative;\n" + 
-    		"    line-height: 15px;\n" +
+    		"    position: absolute;\n" + 
+    		"    line-height: "+BAR_LINE_HEIGHT+"px;\n" +
     		"    border: thin grey solid;\n" +
-    		"    background-color: #ab9;\n" + 
-    		"    margin: 1px 0 0 0;\n" + 
+    		"    background-color: #eee;\n" + 
+    		"    margin: 0;\n" +
+    		"    padding: 0;\n" + 
     		"    font-size: 10px;\n" + 
     		"}\n" + 
-    		".hidden {\n" +
-    		"    display: none;\n" +
-    		"}\n" +
-    		"\n" + 
     		"</style>\n");
   }
   
-  
-//  public String summaryString() {
-//    return summaryString("");
-//  }
-//
-//  private String summaryString(String prefix) {
-//    StringBuffer sb = new StringBuffer();
-//    sb.append(prefix).append(name).append(" start ").append(startMillis).append("ms\n");
-//    for (TimerSummary nestedSummary : nestedSummaries) {
-//      sb.append(nestedSummary.summaryString(prefix + "  "));
-//    }
-//    sb.append(prefix).append(name).append(" stop ").append(stopMillis).append("ms, duration ").append(stopMillis - startMillis).append("ms\n");
-//    return sb.toString();
-//  }
-  
   private void renderChart() {
-    sb.append("<div id=\"chart-stats\">\n" + 
-    		      "Response Time For Request\n");    
-    renderTimerSummary(timerSummary);
+    sb.append("Response Time For Requests\n<div id=\"chart-stats\">\n");    
+    renderTimelineSummary(0, timerSummary);
     sb.append("</div>\n");
   }
 
-  private void renderTimerSummary(TimerSummary summary) { 
-    sb.append("<div class=\"timeline\" style=\"left: ").append(summary.getStartMillis() * 10)
-      .append("px; width: ").append(summary.getStopMillis() * 10).append("px;\">")
-      .append(summary.getName()).append("<div class=\"hidden\">More Details: x,y,z And more</div>");
+  private void renderTimelineSummary(int nestingLevel, TimerSummary summary) { 
+    long durationMillis = summary.getStopMillis() - summary.getStartMillis();
+    int scaleFactor = 10;
+    sb.append("<div class=\"timeline\" style=\"left: ").append(summary.getStartMillis() * scaleFactor)
+      .append("px; top: ").append(nestingLevel * BAR_LINE_HEIGHT)
+      .append("px; width: ").append(durationMillis * scaleFactor)
+      .append("px;\" title=\"[").append(summary.getStartMillis()).append("-").append(summary.getStopMillis()).append("ms] ").append(durationMillis).append("ms\">")
+      .append(summary.getName()).append("</div>");
+    
     for (TimerSummary nestedSummary : summary.getNestedSummaries()) {
-      renderTimerSummary(nestedSummary);
+      renderTimelineSummary(++nestingLevel, nestedSummary);
     }
-    sb.append("</div>\n");
   }
 
 }
